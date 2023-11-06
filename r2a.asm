7b73d6eebc68bd72cc46f502bda9b6d6f5e2de6c
frannjribatto <80211117+frannjribatto@users.noreply.github.com>
2023-11-06 20:40:10 -0300

    r2a

--- r2a.asm
+++ r2a.asm
@@ -4,6 +4,7 @@
 
 .data
 
+	ascii db "000", 0dh, 0ah, 24h
 	multi db 100, 10, 1, 0dh, 0ah, 24h
 
 .code
@@ -12,7 +13,22 @@ public r2a
 
 
 	r2a proc
-	
+	mov ah, 0
+	mov al, bx
+	mov dl, 10
+	mov cx 3
+	mov si, 2
+
+toAscii:
+
+	div dl
+	add ascii[si], ah
+	mov ah, 0
+	dec si
+	loop toAscii
+
+	mov ah, 9
+	mov dx, offset ascii
 
 	ret
 
